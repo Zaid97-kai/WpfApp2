c2701b5d356df18f1dad06449ce83b146c1109ab
Заид Мингалиев <zzmingaliev@outlook.com>
2021-08-23 00:43:48 +0300

    Реализовано добавление

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,18 +22,45 @@ namespace WpfApp2
     public partial class MainWindow : Window
     {
         public ObservableCollection<Student> students = new ObservableCollection<Student>();
+        public ObservableCollection<Group> groups = new ObservableCollection<Group>();
         private DiaryDBEntities _diaryDBEntities = new DiaryDBEntities();
         public MainWindow()
         {
             InitializeComponent();
-            StudentsList.ItemsSource = _diaryDBEntities.Students.ToList();
+            groups.Add(new Group()
+            {
+                Id = new Random().Next(0, 1000),
+                NumberGroup = 4332,
+                YearOfAdmission = new DateTime(2019, 9, 1)
+            });
+            students.Add(new Student()
+            {
+                Id = new Random().Next(0, 1000),
+                Name = "Рахимов Р.Р.",
+                Group = groups[0]
+            });
+            StudentsList.ItemsSource = students;
+            NumberGroupNewStudent.ItemsSource = groups;
+            //StudentsList.ItemsSource = _diaryDBEntities.Students.ToList();
         }
         private void StudentsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            NameEditStudent.Text = ((Student)StudentsList.SelectedItem).Name;
+            NumberGroupEditStudent.Text = ((Student)StudentsList.SelectedItem).Group.NumberGroup.ToString();
         }
 
         private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            Student NewStudent = new Student()
+            {
+                Id = new Random().Next(0, 1000),
+                Name = NameNewStudent.Text,
+                Group = (Group)NumberGroupNewStudent.SelectedItem
+            };
+            this.students.Add(NewStudent);
+        }
+
+        private void EditStudentButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
